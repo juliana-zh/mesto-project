fde79a0cbf7c71e4e1e994c0d3005378d21161d4
Alexander <sashashvetsov3@gmail.com>
2022-04-27 22:10:24 +0300

    add fix

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -13,26 +13,18 @@ export default class PopupWithForm extends Popup {
   }
 
   _getInputValues() {
-    this._profileTitle = document.querySelector('.form__item_el_name');
-    this._profileProfession = document.querySelector('.form__item_el_profession');
-    this._profileAvatar = document.querySelector('.form__item_el_avatar-ref');
-    this._imageName = document.querySelector('.form__item_el_title');
-    this._imageRef = document.querySelector('.form__item_el_ref');
+    this._fieldValues = this._form.querySelectorAll('.form__item')
   }
 
   setEventListeners() {
     super.setEventListeners();
     this._form.addEventListener('submit', evt => {
       this._getInputValues();
-      this._handleSubmitForm(evt, this._profileTitle, this._profileProfession, this._profileAvatar, this._imageName, this._imageRef);
+      this._handleSubmitForm(evt, ...this._fieldValues);
     });
   }
 
   clearInputs() {
-    this._profileTitle.value = "";
-    this._profileProfession.value = "";
-    this._profileAvatar.value = "";
-    this._imageName.value = "";
-    this._imageRef.value = "";
+    this._form.reset()
   }
 }
